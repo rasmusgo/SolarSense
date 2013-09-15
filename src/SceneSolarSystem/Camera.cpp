b5e63ae63f001c87e8593ac041cea1a74c7d77b1
fonserbc <fonserbc@gmail.com>
2013-09-15 21:14:05 +0200

    Smoothed camera control

--- src/SceneSolarSystem/Camera.cpp
+++ src/SceneSolarSystem/Camera.cpp
@@ -13,6 +13,8 @@ void Camera::draw() {
 }
 
 void Camera::update(float deltaTime) {
+    acc = vec3f(0.0f);
+
     //temporal mouseTest (arround a planet)
     if (InputManager::isMouseDown(sf::Mouse::Left)) {
         vec2i mouseDis = InputManager::getMouseDisplacement();
@@ -24,18 +26,46 @@ void Camera::update(float deltaTime) {
         rotM = m*rotM;
     }
 
+    float maxAcc = 5.0f;
     if(InputManager::isKeyDown(sf::Keyboard::W)){
-        pos += vec3f(0.0,0.0,-1.0);
+        acc.z = -maxAcc;
     }
     if(InputManager::isKeyDown(sf::Keyboard::S)){
-        pos += vec3f(0.0,0.0,1.0);
+        acc.z = maxAcc;
     }
     if(InputManager::isKeyDown(sf::Keyboard::D)){
-        pos += vec3f(1.0,0.0,0.0);
+        acc.x = maxAcc;
     }
     if(InputManager::isKeyDown(sf::Keyboard::A)){
-        pos += vec3f(-1.0,0.0,0.0);
+        acc.x = -maxAcc;
     }
+
+    if (glm::length(acc) > 0.1f) {
+        vel += acc*deltaTime;
+    }
+    if (glm::length(vel) > 0.1f) {
+        float friction = 5.0f;
+        if (glm::abs(acc.x) < 0.1) {
+            if (vel.x > 0.1f) vel.x = glm::max(0.0f, vel.x - friction*deltaTime);
+            else if (vel.x < -0.1f) vel.x = glm::min(0.0f, vel.x + friction*deltaTime);
+            else vel.x = 0.0f;
+        }
+        if (glm::abs(acc.y) < 0.1) {
+            if (vel.y > 0.1f) vel.y = glm::max(0.0f, vel.y - friction*deltaTime);
+            else if (vel.y < -0.1f) vel.y = glm::min(0.0f, vel.y + friction*deltaTime);
+            else vel.y = 0.0f;
+        }
+        if (glm::abs(acc.z) < 0.1) {
+            if (vel.z > 0.1f) vel.z = glm::max(0.0f, vel.z - friction*deltaTime);
+            else if (vel.z < -0.1f) vel.z = glm::min(0.0f, vel.z + friction*deltaTime);
+            else vel.z = 0.0f;
+        }
+
+        float maxVel = 10.0f;
+        if (glm::length(vel) > maxVel) vel = glm::normalize(vel)*maxVel;
+    }
+
+    pos += vel*deltaTime + 0.5f*acc*deltaTime*deltaTime;
 }
 
 void Camera::drawHUD() {
