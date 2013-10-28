2f05c3b7cdc87edddebef07ca3f870a532f30c77
fonserbc <fonserbc@gmail.com>
2013-10-28 16:13:22 +0100

    Working on 3D non-parallax plane distance

--- src/Commons.hpp
+++ src/Commons.hpp
@@ -12,7 +12,8 @@ namespace Utils {
         return vec3f(m[3][0],m[3][1],m[3][2]);
     }
 
-    inline vec3f lerp(vec3f& from, vec3f& to, float t) {
+    template <typename T>
+    inline T lerp(T& from, T& to, float t) {
         return to*t + from*(1.0f-t) ;
     }
 }
